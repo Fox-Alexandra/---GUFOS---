06148ee839c4b91a49f4bae55c6390cf5ed48bb6
FIC <FIC@info.local>
2019-10-22 15:45:43 -0300

    API e Banco de Dados do Projeto Gufos

---
+++ GUFOS/Controllers/CategoriaController.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GUFOS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GUFOS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class CategoriaController : ControllerBase
+    {
+        GufosContext context = new GufosContext(); //GUFOScontext é renspopnsavel pela conexao como banco
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Categoria>>> Get()
+        {
+            List<Categoria> listaDeCategoria = await context.Categoria.ToListAsync();
+
+            if(listaDeCategoria == null)
+            {
+                return NotFound();
+            }
+            return listaDeCategoria;
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Categoria>> Get(int id)
+        {
+            Categoria categoriaRetornada = await context.Categoria.FindAsync(id); //caso nao encontre o id
+            if (categoriaRetornada == null)
+            {
+                return NotFound();
+            }
+            return categoriaRetornada;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Categoria /*define o tipo de retorno*/>> Post(Categoria categoria) 
+        {
+            try //usa para tentar algo/retornar erros
+            {
+                await context.Categoria.AddAsync(categoria); 
+                await context.SaveChangesAsync();
+            }
+            catch (System.Exception)
+            {
+                    
+                throw;
+            }
+                
+            return categoria;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, Categoria categoria)
+        {
+            Categoria categoriaRetornada = await context.Categoria.FindAsync(id);
+            if(categoriaRetornada == null){
+                return NotFound();
+            }
+            categoriaRetornada.Titulo = categoria.Titulo;
+            context.Categoria.Update(categoriaRetornada);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete ("{id}")]
+        public async Task<ActionResult<Categoria>> Delete(int id){
+            Categoria categoriaRetornada = await context.Categoria.FindAsync(id);
+            if(categoriaRetornada == null){
+                return NotFound();
+            }
+            context.Categoria.Remove(categoriaRetornada);
+            await context.SaveChangesAsync();
+
+            return categoriaRetornada;
+        }
+    }
+}
